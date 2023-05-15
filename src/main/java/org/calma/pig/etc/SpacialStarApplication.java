6d99380e7c7f75ddfb407bfbbc4d9d5ea4ad5df6
Maxence Guidez <202130087@collegealma.ca>
2023-05-15 16:04:02 -0400

    In the ControlPanel, it's well display.

--- src/main/java/org/calma/pig/etc/SpacialStarApplication.java
+++ src/main/java/org/calma/pig/etc/SpacialStarApplication.java
@@ -2,6 +2,7 @@ package org.calma.pig.etc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
@@ -19,11 +20,14 @@ public class SpacialStarApplication extends Application {
 
         SplitPane root = new SplitPane(controlPanel, grid);
         root.setOrientation(Orientation.VERTICAL);
+        root.setPadding(new Insets(0));
 
         Scene scene = new Scene(root, 960, 540);
         stage.getIcons().add(new Image("org/calma/pig/etc/images/logo.png"));
         stage.setScene(scene);
         stage.setResizable(true);
+        stage.setMinWidth(500);
+        stage.setMinHeight(400);
         stage.setTitle("Étoile spaciale");
         stage.show();
     }

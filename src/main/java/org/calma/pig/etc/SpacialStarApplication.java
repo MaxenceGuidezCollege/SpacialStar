95fb52f69fd70caec4caa12f50983d5fd91fcbc4
Maxence Guidez <202130087@collegealma.ca>
2023-05-15 13:24:42 -0400

    Add a ControlPanel in the top part. (SceneBuilder)

--- src/main/java/org/calma/pig/etc/SpacialStarApplication.java
+++ src/main/java/org/calma/pig/etc/SpacialStarApplication.java
@@ -1,20 +1,23 @@
 package org.calma.pig.etc;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class SpacialStarApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        AnchorPane panneauControl = new AnchorPane();
+        GridPane controlPanel = (GridPane) FXMLLoader
+                .load(getClass().getResource("fxml/controlPanel.fxml"));
+
         Grid grid = new Grid(150); //150 is the recommended value.
 
-        SplitPane root = new SplitPane(panneauControl, grid);
+        SplitPane root = new SplitPane(controlPanel, grid);
         root.setOrientation(Orientation.VERTICAL);
 
         Scene scene = new Scene(root, 960, 540);
---
+++ src/main/java/org/calma/pig/etc/controllers/ControlPanelController.java
@@ -0,0 +1,57 @@
+package org.calma.pig.etc.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControlPanelController {
+    @FXML
+    private TextField oneX;
+    @FXML
+    private TextField oneY;
+
+    @FXML
+    private TextField twoX;
+    @FXML
+    private TextField twoY;
+
+    @FXML
+    private TextField threeX;
+    @FXML
+    private TextField threeY;
+
+    @FXML
+    private TextField fourX;
+    @FXML
+    private TextField fourY;
+
+    @FXML
+    private TextField fiveX;
+    @FXML
+    private TextField fiveY;
+
+    @FXML
+    private Button rotDown;
+    @FXML
+    private Button rotUp;
+
+
+    @FXML
+    private Button xDown;
+    @FXML
+    private Button xUp;
+    @FXML
+    private Button yDown;
+    @FXML
+    private Button yUp;
+
+    @FXML
+    private Button homDown;
+    @FXML
+    private Button homUp;
+
+    @FXML
+    void initialize(){
+
+    }
+}
